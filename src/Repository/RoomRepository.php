474d5ab13316949a5fe8ea97fa82b8245015186b
Vladislav <goalbuff@yandex.ru>
2026-08-01 16:41:47 +0300

    feat: remove unnecessary feature as there is no need for dynamic files

--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -7,95 +7,103 @@ namespace App\Repository;
 use App\Entity\Room;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
 
 /**
  * @extends ServiceEntityRepository<Room>
  */
 class RoomRepository extends ServiceEntityRepository
 {
-    private array $rooms = [
-        [
-            'id' => 1,
-            'name' => 'Коворкинг',
-            'description' => '',
-            'attachments' => [
-                'path' => '/assets/images/rooms/coworking.jpg',
-            ],
-            'attributes' => [
-                [
-                    'type' => 'seats',
-                    'count' => 3,
-                    'capacity' => 5,
-                ],
-                [
-                    'type' => 'boards',
-                    'count' => 1,
-                ],
-                [
-                    'type' => 'air-conditioners',
-                    'count' => 1,
-                ],
-            ],
-        ],
-        [
-            'id' => 2,
-            'name' => 'Большая переговорная',
-            'description' => 'Большая комната в ',
-            'attachments' => [
-                'path' => '/assets/images/rooms/big.jpg',
-            ],
-            'attributes' => [
-                [
-                    'type' => 'seats',
-                    'count' => 10,
-                    'capacity' => 10,
-                ],
-                [
-                    'type' => 'displays',
-                    'count' => 1,
-                ],
-                [
-                    'type' => 'tables',
-                    'count' => 1,
-                ],
-                [
-                    'type' => 'air-conditioners',
-                    'count' => 1,
-                ],
-            ],
-        ],
-        [
-            'id' => 3,
-            'name' => 'Малая переговорная',
-            'description' => 'Малая комната в ',
-            'attachments' => [
-                '/assets/images/rooms/small.jpg',
-            ],
-            'attributes' => [
-                [
-                    'type' => 'seats',
-                    'count' => 9,
-                    'capacity' => 9,
-                ],
-                [
-                    'type' => 'tables',
-                    'count' => 1,
-                ],
-                [
-                    'type' => 'air-conditioners',
-                    'count' => 1,
-                ],
-            ],
-        ],
-    ];
 
-    public function __construct(ManagerRegistry $registry)
-    {
+    public function __construct(
+        ManagerRegistry $registry,
+        private readonly Packages $package
+    ) {
         parent::__construct($registry, Room::class);
     }
 
     public function findAll(): array
     {
-        return $this->rooms;
+        $rooms = [
+            [
+                'id' => 1,
+                'name' => 'Коворкинг',
+                'description' => '',
+                'attachments' => [
+                    $this->package->getUrl('images/rooms/coworking.jpg'),
+//                    '/images/rooms/coworking.jpg',
+                ],
+                'attributes' => [
+                    [
+                        'type' => 'seats',
+                        'count' => 3,
+                        'capacity' => 5,
+                    ],
+                    [
+                        'type' => 'boards',
+                        'count' => 1,
+                    ],
+                    [
+                        'type' => 'air-conditioners',
+                        'count' => 1,
+                    ],
+                ],
+            ],
+            [
+                'id' => 2,
+                'name' => 'Большая переговорная',
+                'description' => 'Большая комната в ',
+                'attachments' => [
+                    $this->package->getUrl('images/rooms/big.jpg'),
+//                    '/assets/images/rooms/big.jpg',
+                ],
+                'attributes' => [
+                    [
+                        'type' => 'seats',
+                        'count' => 10,
+                        'capacity' => 10,
+                    ],
+                    [
+                        'type' => 'displays',
+                        'count' => 1,
+                    ],
+                    [
+                        'type' => 'tables',
+                        'count' => 1,
+                    ],
+                    [
+                        'type' => 'air-conditioners',
+                        'count' => 1,
+                    ],
+                ],
+            ],
+            [
+                'id' => 3,
+                'name' => 'Малая переговорная',
+                'description' => 'Малая комната в ',
+                'attachments' => [
+                    $this->package->getUrl('images/rooms/small.jpg'),
+//                    '/assets/images/rooms/small.jpg',
+                ],
+                'attributes' => [
+                    [
+                        'type' => 'seats',
+                        'count' => 9,
+                        'capacity' => 9,
+                    ],
+                    [
+                        'type' => 'tables',
+                        'count' => 1,
+                    ],
+                    [
+                        'type' => 'air-conditioners',
+                        'count' => 1,
+                    ],
+                ],
+            ],
+        ];
+
+        return $rooms;
     }
 }
