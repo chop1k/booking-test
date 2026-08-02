f4e8601df7a7779a8a0bfad1e137f8b9515ed427
Vladislav <goalbuff@yandex.ru>
2026-08-02 20:53:19 +0300

    fix: oops forgot to rename env

--- src/Security/TGAAuthenticator.php
+++ src/Security/TGAAuthenticator.php
@@ -20,7 +20,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPasspor
 class TGAAuthenticator extends AbstractAuthenticator
 {
     public function __construct(
-        #[Autowire(env: 'TELEGRAM_BOT_TOKEN')]
+        #[Autowire(env: 'POLLING_TELEGRAM_TOKEN')]
         private readonly string $botToken,
 
         private readonly UserRepository $userRepository,
